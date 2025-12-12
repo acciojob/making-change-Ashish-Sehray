debaccc041f104507ff5a50cd4574f8179c64e1c
Ashish-Sehray <sehrayashish54@gmail.com>
2025-12-12 09:21:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 const makeChange = (c) => {
   // your name here
+	let obj={};
+	if(c >= 25){
+		obj.q = Math.floor(c/25);
+		c= c%25;
+	}else{
+		obj.q=0;
+	}
+	if(c<25 && c>=10){
+		obj.d = Math.floor(c/10);
+		c= c%10;
+	}else{
+		obj.d=0;
+	}
+	if(c<10 && c>=5){
+		obj.n = Math.floor(c/5);
+		c= c%5;
+	}else{
+		obj.n=0;
+	}
+	if(c<5 && c>=1){
+		obj.p = Math.floor(c/1);
+		
+	}else{
+		obj.p=0;
+	}
+	return obj;
 };
 
 // Do not the change the code below
